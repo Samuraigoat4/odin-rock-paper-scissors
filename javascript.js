54fcaa713800826eccb4db42853bceb1af07f869
samuraigoat4 <rickjj84@gmail.com>
2022-11-16 17:18:25 -0500

    Edit variable from choice to computerSelection, for easy reading later

--- javascript.js
+++ javascript.js
@@ -2,16 +2,17 @@ console.log("Good Luck!");
 
 let getComputerChoice = Math.floor(Math.random() * 30);
 let choice = "";
+let computerSelection = "";
 //console.log(getComputerChoice);
 
 //convert number to R,P,or S
 if (getComputerChoice <= 10) {
-    choice = ("Rock")
+    computerSelection = ("rock")
 } else if (getComputerChoice <= 20 && getComputerChoice >= 11) {
-    choice = ("Paper")
+    computerSelection = ("paper")
 } else if (getComputerChoice >= 21) {
-    choice = ("Scissors")
+    computerSelection = ("scissors")
 }
-//console.log(choice);
+console.log(computerSelection);
 
 //Play the game once
